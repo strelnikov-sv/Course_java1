c83fbfe7d523b158158682376429067bd869e34b
Sergey <strelnikov-sv@yandex.ru>
2020-03-26 12:28:17 +0300

    Paint

--- Homework_7/src/ru/geekbrains/homework_7/FieldPanel.java
+++ Homework_7/src/ru/geekbrains/homework_7/FieldPanel.java
@@ -2,17 +2,35 @@ package ru.geekbrains.homework_7;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
 
 public class FieldPanel extends JPanel {
     public static final int MODE_HVH = 0;
     public static final int MODE_HVA = 1;
 
+    private static final int WINDOW_WIDTH = 500; //???
+
     FieldPanel() {
-        setBackground(Color.BLUE);
+        setBackground(Color.BLUE); // ??, пропадает цвет
     }
 
     public void startNewGame(int gameMode, int fieldSizeX, int fieldSizeY, int winLength) {
         System.out.printf("damn mode: %d\nfield size: %d,\nwin length: %d\n", gameMode, fieldSizeX, winLength);
+    }
+
+    //Пока просто нарисовал, без привязки
+    public void paint(Graphics g) {
+        int hor;
+        int vert;
+        int grid = 3;
+        int size_grid = WINDOW_WIDTH / grid;
+//      super.paint(g);
+        Graphics2D graph = (Graphics2D) g;
+        graph.setPaint(new Color(20, 200, 20));
+        for (hor = size_grid; hor <= WINDOW_WIDTH; hor += size_grid) {
+            for (vert = size_grid; vert <= WINDOW_WIDTH; vert += size_grid)
+                g.drawRect(0, 0, hor, vert);
+        }
     }
 }
 
